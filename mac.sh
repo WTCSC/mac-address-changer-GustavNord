7324f1cf0093e1f8e71e1a66e4ee3d1fc9950661
GustavNord <gus.nordqvist@gmail.com>
2025-01-21 22:50:43 -0700

    update

--- mac.sh
+++ mac.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-INTERFACE=$1
-NEW_MAC=$2
+check_mac() {
+    local mac=$1
+    if [[ $mac =~ ^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$ ]]; then
+        return 0
+    else
+        echo "Error: The MAC Address isn't in the correct format."
+        echo "Format should be: ##:##:##:##:##:##"
+        exit 1
+    fi
+}
