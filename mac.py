52a5db8b896dd629305bce6b7511713f775945fa
Gustav Nordqvist <gus.nordqvist@gmail.com>
2025-01-27 13:57:29 -0700

    started

--- mac.py
+++ mac.py
@@ -1,2 +1,11 @@
+network_interface = 0
+new_mac_address = 0 
+user_input0 = 0
+
 def help():
-    print("How to use this script:\n")
+    print("How to use this script:")
+    print(user_input0 network_interface, new_mac_address)
+    print()
+
+def check_mac():
+    
