e40bce6475956fa9fc7ef3e4ef6d56275a91c9f8
Gustav Nordqvist <gus.nordqvist@gmail.com>
2025-01-29 12:47:15 -0700

    updated

--- mac.py
+++ mac.py
@@ -1,19 +1,30 @@
 import subprocess
-import string
-import random
+import sys
 import re
-
-
-
-
-network_interface = 0
-new_mac_address = 0 
-user_input0 = 0
+import os
 
 def help():
     print("How to use this script:")
-    print(user_input0 network_interface, new_mac_address)
-    print()
+    print(f"{sys.argv[0]} <network_interface> <new_mac_address>")
+    print("Example: {} eth0 00:1A:2B:3C:4D:5E".format(sys.argv[0]))
+    print("Example for random MAC: {} eth0 random">format(sys.argv[0]))
+    print("Example to reset MAC address: {} eth0 reset".format(sys.argv[0]))
+    sys.exit(1)
 
-def check_mac():
-    
+
+def check_mac(mac):
+    if re.match(r'^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$', mac):
+        return True
+    else:
+        print("Error: The MAC Address isn't in the correct format.")
+        print("Format should be: 00:1A:2B:3C:4D:5E")
+        sys.exit(1)
+
+if len(sys.argv) != 3:
+    print("Error: Invalid number of arguments.")
+    help()
+
+interface = sys.argv[1]
+new_mac = sys.argv[2]
+user_input0 = sys.argv[3]
+
